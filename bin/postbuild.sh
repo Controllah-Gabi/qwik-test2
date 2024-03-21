d10f54d184327e5b36da5aad0d02a0341f9617fb
HartThomas <t.j.hart89@hotmail.co.uk>
2024-03-21 12:04:45 +0000

    Edit entry point of server

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -6,6 +6,7 @@ mkdir -p ./.amplify-hosting/compute
 
 cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+cp -r ./server ./.amplify-hosting/compute/default/server
 
 cp -r public ./.amplify-hosting/static
 
